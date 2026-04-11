34c23f592a69bcc431209f4d3881305bed66e1ee
yansss <2316676216@qq.com>
2026-04-11 22:58:17 +0800

    新增测试kvstore读写qps的代码

---
+++ testcase/testcase_sample/test_get_qps.c
@@ -0,0 +1,201 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+// *************************************** SET for 10000 times ******************
+// **************** testcase for log module ****************
+#define MAX_MSG_LENGTH		1024
+#define TIME_SUB_MS(tv1, tv2)  ((tv1.tv_sec - tv2.tv_sec) * 1000 + (tv1.tv_usec - tv2.tv_usec) / 1000)
+
+int send_msg(int connfd, char *msg, int length) {
+
+	int res = send(connfd, msg, length, 0);
+	if (res < 0) {
+		perror("send");
+		exit(1);
+	}
+	return res;
+}
+
+int recv_msg(int connfd, char *msg, int length) {
+
+	int res = recv(connfd, msg, length, 0);
+	if (res < 0) {
+		perror("recv");
+		exit(1);
+	}
+	return res;
+
+}
+
+
+
+
+void testcase(int connfd, char *msg, char *pattern, char *casename) {
+
+	if (!msg || !pattern || !casename) return ;
+	char new_msg[MAX_MSG_LENGTH] = {0};
+	int total_length = snprintf(new_msg, MAX_MSG_LENGTH, "%ld*%s", strlen(msg), msg);
+	//send_msg(connfd, msg, strlen(msg));
+	send_msg(connfd, new_msg, total_length);
+
+	char result[MAX_MSG_LENGTH] = {0};
+	recv_msg(connfd, result, MAX_MSG_LENGTH);
+
+	if (strcmp(result, pattern) == 0) {
+		//printf("==> PASS ->  %s\n", casename);
+	} else {
+		printf("==> FAILED -> %s, '%s' != '%s' \n", casename, result, pattern);
+		exit(1);
+	}
+
+}
+
+
+
+int connect_tcpserver(const char *ip, unsigned short port) {
+
+	int connfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(struct sockaddr_in));
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(ip);
+	server_addr.sin_port = htons(port);
+
+	if (0 !=  connect(connfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr_in))) {
+		perror("connect");
+		return -1;
+	}
+	
+	return connfd;
+	
+}
+
+void rbtree_testcase(int connfd, int count) {
+
+	int i = 0;
+
+	struct timeval tv_begin;
+	gettimeofday(&tv_begin, NULL);
+
+	for (i = 0;i < count;i ++) {
+
+		char cmd[128] = {0};
+		snprintf(cmd, 128, "RGET Teacher%d King%d", i, i);
+        char reply[128] = {0};
+        snprintf(reply, 128, "King%d\r\n", i);
+		testcase(connfd, cmd, reply, cmd);
+	}
+	struct timeval tv_end;
+	gettimeofday(&tv_end, NULL);
+
+	int time_used = TIME_SUB_MS(tv_end, tv_begin); // ms
+
+	printf("rbtree testcase --> time_used: %d, qps: %d\n", time_used, (count * 1000 / time_used));
+
+
+}
+void array_testcase(int connfd, int count) {
+
+	int i = 0;
+
+	struct timeval tv_begin;
+	gettimeofday(&tv_begin, NULL);
+	for (i = 0;i < count;i ++) {
+
+		char cmd[128] = {0};
+		snprintf(cmd, 128, "GET Teacher%d King%d", i, i);
+        char reply[128] = {0};
+        snprintf(reply, 128, "King%d\r\n", i);
+		testcase(connfd, cmd, reply, cmd);
+	}
+	struct timeval tv_end;
+	gettimeofday(&tv_end, NULL);
+
+	int time_used = TIME_SUB_MS(tv_end, tv_begin); // ms
+
+	printf("array testcase --> time_used: %d, qps: %d\n", time_used, (count * 1000 / time_used));
+
+}
+void hash_testcase(int connfd, int count) {
+
+	int i = 0;
+
+	struct timeval tv_begin;
+	gettimeofday(&tv_begin, NULL);
+
+	for (i = 0;i < count;i ++) {
+
+		char cmd[128] = {0};
+		snprintf(cmd, 128, "HGET Teacher%d King%d", i, i);
+        char reply[128] = {0};
+        snprintf(reply, 128, "King%d\r\n", i);
+		testcase(connfd, cmd, reply, cmd);
+	}
+
+	struct timeval tv_end;
+	gettimeofday(&tv_end, NULL);
+
+	int time_used = TIME_SUB_MS(tv_end, tv_begin); // ms
+
+	printf("hash testcase --> time_used: %d, qps: %d\n", time_used, (count * 1000 / time_used));
+}
+
+void skiplist_testcase(int connfd, int count) {
+
+	int i = 0;
+
+	struct timeval tv_begin;
+	gettimeofday(&tv_begin, NULL);
+
+	for (i = 0;i < count;i ++) {
+
+		char cmd[128] = {0};
+		snprintf(cmd, 128, "LGET Teacher%d King%d", i, i);
+        char reply[128] = {0};
+        snprintf(reply, 128, "King%d\r\n", i);    
+		testcase(connfd, cmd, reply, cmd);
+	}
+	struct timeval tv_end;
+	gettimeofday(&tv_end, NULL);
+
+	int time_used = TIME_SUB_MS(tv_end, tv_begin); // ms
+
+	printf("skiplist testcase --> time_used: %d, qps: %d\n", time_used, (count * 1000 / time_used));
+
+}
+void testcase_(int connfd, int count){
+
+	int i = 0;
+
+	rbtree_testcase(connfd, count);
+	skiplist_testcase(connfd, count);
+	array_testcase(connfd, count);
+	hash_testcase(connfd, count);
+}
+
+// testcase 192.168.243.131  2000 mode: 0 for rbtree, 1 for array, 2 for hash 3 for skiplist
+int main(int argc, char *argv[]) {
+
+	if (argc < 3) {
+		printf("arg error\n");
+		return -1;
+	}
+
+	char *ip = argv[1];
+	int port = atoi(argv[2]);
+
+	int connfd = connect_tcpserver(ip, port);
+	int count = 100000;
+    if(argc == 3){
+		testcase_(connfd, count);
+	}
+
+
+	return 0;
+	
+}
