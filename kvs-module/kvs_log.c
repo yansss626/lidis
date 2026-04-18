08cfa6913f80bc075d959e482777970db3f21171
yansss <2316676216@qq.com>
2026-04-18 23:49:02 +0800

    修改kvsore增量持久化(AOF)的逻辑： 1：加载/读取.aof文件时，通过mmap将磁盘数据映射到内存空间中，之后读取.aof文件直接通过共享内存操作。 2：写入.aof文件（数据落盘）通过io_uring：每次成功地修改了内存数据后，每次通过调用io_uring_prep_write，逐次地写入。

--- kvs-module/kvs_log.c
+++ kvs-module/kvs_log.c
@@ -3,130 +3,187 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <liburing.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
-static FILE * fp_log = NULL;
+#define ENTRY_LENGTH 1024
+
+static int fd_log = -1;
+
+static struct io_uring ring = {0};
+static int ring_inited = 0;
+
 
 static msg_handler kvs_handler;
 
 extern kvs_conf_t global_config;
 
-// int kvs_log_read(FILE * fp){
-//     if(fp == NULL) return -1;
 
-//     client_info cli = {0};
-//     cli.w_cap = 128; 
-//     cli.wbuf = (char *)malloc(cli.w_cap);
-//     if(cli.wbuf == NULL) return -1;
-
-//     cli.rbuf = (char *)malloc(4096);
-//     if(cli.rbuf == NULL) {
-//         free(cli.wbuf);
-//         return -1;
-//     }
-//     memset(cli.rbuf, 0, 4096);
-//     cli.r_cap = 4096;
-    
-//     while(fgets(cli.rbuf + cli.r_pos, cli.r_cap - cli.r_pos - 1, fp) != NULL){
-//         cli.r_pos += strlen(cli.rbuf + cli.r_pos);
-        
-
-//         if(cli.r_pos >=2 && cli.rbuf[cli.r_pos - 2] == '\r' && cli.rbuf[cli.r_pos - 1] == '\n'){
-            
-//             cli.rbuf[cli.r_pos - 2] = '\0';    
-//             //printf("%s\n", cli.rbuf);
-//             kvs_handler(&cli);
-//             cli.r_pos = 0;
-//             continue;
-//         }
-//         if(cli.r_cap - cli.r_pos < 128){
-//             cli.r_cap *= 2;
-//             char * temp  = (char *)realloc(cli.rbuf, cli.r_cap);
-//             if(temp == NULL) break;
-//             cli.rbuf = temp;
-//         }
-       
+typedef struct log_write_ctx_s{
+    char * buf;
+    size_t len;
+}log_write_ctx;
 
 
-//     }
-
-//     free(cli.wbuf);
-//     free(cli.rbuf);
-//     return 0;
-// }
-
-int kvs_log_read(FILE * fp){
-    if(global_config.enable_log == 0) return 0;
-    if(fp == NULL) return -1;
-
+int kvs_log_read(char * ptr, size_t size){
+    if(ptr == NULL || size <= 0) return -1;
     client_info cli = {0};
-    cli.w_cap = 128 - 1; 
-    cli.wbuf = (char *)malloc(128);
-    if(cli.wbuf == NULL) return -1;
 
-    cli.r_cap = 4096 - 1; 
-    cli.rbuf = (char *)malloc(4096);
+    cli.w_cap = BUFFER_SIZE; 
+    cli.wbuf = (char *)malloc(BUFFER_SIZE + 1);
+    if(cli.wbuf == NULL) return -2;
+
+    cli.r_cap = BUFFER_SIZE; 
+    cli.rbuf = (char *)malloc(BUFFER_SIZE + 1);
     if(cli.rbuf == NULL) {
         free(cli.wbuf);
-        return -1;
+        return -2;
     }
 
-    int payload_length = 0;
-    while(fscanf(fp, "%d*", &payload_length) == 1){
-        if(cli.r_cap < payload_length){
-            char * temp = (char *)realloc(cli.rbuf, payload_length + 1);
+
+
+    int pos = 0;
+    int total_len = 0;
+    while(pos < size){
+        int head_len = 0;
+        total_len = resp_parse_bulk_size(ptr, size - total_len, &head_len);
+        if(total_len <= 0){
+            printf("Log file error\n");
+            break;
+        }
+        if(total_len > cli.r_cap){
+            char * temp = (char *)realloc(cli.rbuf, total_len + 1);
             if(temp == NULL){
                 break;
             }
             cli.rbuf = temp;
-            cli.r_cap = payload_length;
+            cli.r_cap = total_len;            
         }
+        cli.cmd_hl = head_len;
+        memcpy(cli.rbuf , ptr, total_len);
+        cli.rbuf[total_len] = '\0';
+        //printf("cli.rbuf: %s\n", cli.rbuf);
+        kvs_handler(&cli);
 
-        int len = fread(cli.rbuf, 1, payload_length, fp);
-        if(len == payload_length){
-            cli.rbuf[payload_length] = '\0';
-            kvs_handler(&cli);
-        }
+        pos += total_len;
+        ptr += total_len;
 
     }
+
     free(cli.rbuf);
     free(cli.wbuf);
+
     return 0;
 }
 
+int kvs_uring_init(){
+    int ret = io_uring_queue_init(ENTRY_LENGTH, &ring, 0);
+    if(ret < 0){
+        fprintf(stderr, "io_uring_queue_init error: %s\n", strerror(-ret));
+        return -1;
+    }
+    ring_inited = 1;
+    return 0;
 
-
-
+}
 
 int kvs_log_init(msg_handler handler){
     if(global_config.enable_log == 0) return 0;
+    fd_log = open("./kvs-module/kvs_appendonly.aof", O_RDWR | O_CREAT | O_APPEND, 0644);
+    if(fd_log < 0){
+        perror("open");
+        return -1;
+    }
+    kvs_uring_init();
     kvs_handler = handler;
+    struct stat statbuf = {0};
+    fstat(fd_log, &statbuf);
+    if(statbuf.st_size <= 0) return 0;
+    
+    char * ptr = (char *)mmap(NULL, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd_log, 0);
+    if(ptr == MAP_FAILED){
+        perror("mmap");
+        close(fd_log);
+        fd_log = -1;
+        return -1;
+    }
+    madvise(ptr, statbuf.st_size, MADV_SEQUENTIAL);
+    kvs_log_read(ptr, statbuf.st_size);
 
-    fp_log = fopen("./kvs-module/kvs_appendonly.aof", "a+");
-    if(fp_log == NULL) return -1;
-    fseek(fp_log, 0, SEEK_SET);
-    kvs_log_read(fp_log);
+    munmap(ptr, statbuf.st_size);
+
+
+    
 
     return 0;
-
 }
 
 
 
 int kvs_log_write(client_info * cli){
     if(global_config.enable_log == 0) return 0;
-    if (cli == NULL) return -1;
-    if (fp_log == NULL) return -2;
-    fprintf(fp_log, "%s\r\n", cli->rbuf);
-    fflush(fp_log);
+    if(cli == NULL) return -1;
+    if(fd_log < 0 || ring_inited != 1) return -2;
+    struct io_uring_cqe * cqe = NULL;
+    
+    while(io_uring_peek_cqe(&ring, &cqe) == 0){
+        log_write_ctx * ctx = (log_write_ctx *)io_uring_cqe_get_data(cqe);
+        free(ctx->buf);
+        free(ctx);
+        ctx = NULL;
+        io_uring_cqe_seen(&ring, cqe);
+    }
+    
+    struct io_uring_sqe * sqe = io_uring_get_sqe(&ring);
+    if(sqe == NULL){
+        io_uring_submit(&ring);
+        return -4;
+    }
+    log_write_ctx * ctx = (log_write_ctx *)malloc(sizeof(log_write_ctx));
+    if(ctx == NULL){
+        perror("malloc");
+        return -3;
+    }
+    ctx->buf = (char *)malloc(cli->cmd_tl);
+    if(ctx->buf == NULL){
+        perror("malloc");
+        free(ctx);
+        return -3;
+    }
+    
+    ctx->len = cli->cmd_tl;
+    memcpy(ctx->buf, cli->rbuf, cli->cmd_tl);
+    //printf("ctx->buf: %s\n", ctx->buf);
+    
+    io_uring_prep_write(sqe, fd_log, ctx->buf, ctx->len, 0);
+    io_uring_sqe_set_data(sqe, ctx);
+    io_uring_submit(&ring);
+
+
+
     return 0;
 }
 
 int kvs_log_close(){
     if(global_config.enable_log == 0) return 0;
-    if(fp_log != NULL) fclose(fp_log);
+    if(ring_inited == 1){
+        io_uring_submit(&ring);
+        struct io_uring_cqe * cqe = NULL;
+        while(io_uring_peek_cqe(&ring, &cqe) == 0){
+            log_write_ctx * ctx = io_uring_cqe_get_data(cqe);
+            free(ctx->buf);
+            free(ctx);
+            ctx = NULL;
+            io_uring_cqe_seen(&ring, cqe);
+        }        
+        io_uring_queue_exit(&ring);
+    } 
+    if(fd_log >= 0) close(fd_log);
     return 0;
 }
-
