5c78639c4f47278662c0396b0972c85a995a1b80
yansss <2316676216@qq.com>
2026-04-19 23:56:56 +0800

    修改kvsore全量持久化(SAVE)的逻辑： 1：加载/读取.aof文件时，通过mmap将磁盘数据映射到内存空间中，之后读取.rdb文件直接通过共享内存操作。 2：写入.aof文件（数据落盘）通过io_uring：每一次写入都是覆盖写，遍历底层数据结构，通过io_uring_prep_write提交任务，遍历完引擎后，通过while循环判断所有任务是否完成。

--- kvs-module/kvs_save.c
+++ kvs-module/kvs_save.c
@@ -1,13 +1,20 @@
-
 #include <stdio.h>
 #include "kvstore.h"
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <liburing.h>
+#include <unistd.h>
+#include <sys/mman.h>
+static struct io_uring ring_save = {0};
+static int ring_save_inited = 0;
 
 extern kvs_conf_t global_config;
 
 #define BUFFER_SIZE 1024
-
+#define ENTRY_LENGTH 1024
 static msg_handler kvs_handler;
 
 #if ENABLE_ARRAY
@@ -26,55 +33,212 @@ extern kvs_hash_t global_hash;
 extern kvs_skiplist_t global_skiplist;
 #endif
 
+typedef enum kvs_engine_type_t{
+    KVS_ARRAY,
+    KVS_HASH,
+    KVS_RBTREE,
+    KVS_SKIPLIST,
+}kvs_engine_type;
+
+
+
+static int tasks_count = 0; // number of io_uring_prep_write
+
+int kvs_save_handler(client_info * cli){
+    if(cli == NULL) return -1;
+    char *tokens[KVS_MAX_TOKENS] = {0};
+	int count = kvs_split_token(cli->rbuf + cli->cmd_hl, tokens);
+	if (count < 0) return -2;
+
+    char type = tokens[0][0];
+    char * key = tokens[1];
+    char * value = tokens[2];
+    int length = 0;
+    
+    switch (type)
+    {
+    case 'A':
+        length = kvs_array_set(&global_array, key, value);
+        break;
+    case 'H':
+        length = kvs_hash_set(&global_hash, key, value);
+        break;
+    case 'R':
+        length = kvs_rbtree_set(&global_rbtree, key, value);
+        break;
+    case 'S':
+        length = kvs_skiplist_set(&global_skiplist, key, value);
+        break;
+    
+    default:
+        break;
+    }
+
+    return length;
+}
+
+
 int kvs_save_init(msg_handler handler){
     if(global_config.enable_save == 0) return 0; 
-    kvs_handler = handler;
-    FILE * fp = fopen("./kvs-module/kvs_dump.rdb", "r"); 
-    if(fp == NULL) return -2;
+    int fd = open("./kvs-module/kvs_dump.rdb", O_RDONLY);
+    struct stat statbuf = {0};
+    fstat(fd, &statbuf);
+    if(statbuf.st_size <= 0) return 0;
+    char * ptr = (char *)mmap(NULL, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if(ptr == MAP_FAILED){
+        perror("mmap");
+        close(fd);
+        return -2;
+    }
 
-    kvs_save_read(fp);
-    fclose(fp);
+    kvs_file_read(ptr, statbuf.st_size, kvs_save_handler);
+    munmap(ptr, statbuf.st_size);
 
     return 0;
 }
 
+int kvs_io_uring_write(int fd, char * key, char * value, kvs_engine_type type){
+    if(fd < 0 || key == NULL || value == NULL) return -1;
+    int ret = 0;
+
+
+    io_write_ctx * ctx = (io_write_ctx *)malloc(sizeof(io_write_ctx));
+    if(ctx == NULL) {
+        perror("malloc error");
+        ret = -2;
+        goto cleanup;
+    }
+    memset(ctx, 0, sizeof(io_write_ctx));
+
+    char * engine = NULL;
+    switch (type)
+    {
+    case KVS_ARRAY:{
+        engine = "ARRAY";
+        break;
+    }
+    case KVS_HASH:
+        engine = "HASH";
+        break;
+    case KVS_RBTREE:
+        engine = "RBTREE";
+        break;
+    case KVS_SKIPLIST:
+        engine = "SKIPLIST";
+        break;    
+    default:
+        break;
+    }
+    if(engine == NULL) {
+        ret = -3;
+        printf("kvs_engine_type do not exist\n");
+        goto cleanup;
+    }
+
+    // *3\r\n$<length>\r\n$<length>\r\n<key>\r\n$<length>\r\n<value>\r\n
+
+    int kstr_len = strlen(key); // key string len
+    int vstr_len = strlen(value); // value string len 
+    int engine_len = strlen(engine); // engine string len   
+
+    // 固定开销：*3\r\n + 三个$的bulk头 + 三个\r\n末尾 + length所占字节长度， 保险给64
+    int max_len = 64 + kstr_len + vstr_len + engine_len;
+    
+    ctx->buf = (char *)malloc(max_len);
+    if(ctx->buf == NULL){
+        ret = -2;
+        perror("malloc error");
+        goto cleanup;
+    }
+    
+    int total_len = snprintf(ctx->buf, max_len, "*3\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n", engine_len, engine, 
+            kstr_len, key, vstr_len, value);
+    ctx->len = total_len;
+    //printf("ctx->buf: %s\n", ctx->buf);
+
+    struct io_uring_sqe * sqe = io_uring_get_sqe(&ring_save);
+    if(sqe == NULL){
+        struct io_uring_cqe * cqe = NULL;
+        while(io_uring_peek_cqe(&ring_save, &cqe) == 0){
+            io_write_ctx * ctx = (io_write_ctx *)io_uring_cqe_get_data(cqe);
+            free(ctx->buf);
+            free(ctx);
+            io_uring_cqe_seen(&ring_save, cqe);
+            --tasks_count;
+        }
+        io_uring_submit(&ring_save);
+
+        sqe = io_uring_get_sqe(&ring_save);
+        if(sqe == NULL){
+            printf("sqe == NULL\n");
+            ret = -4;
+            goto cleanup;
+            
+        } 
+    }
+    ++tasks_count;
+    io_uring_prep_write(sqe, fd, ctx->buf, total_len, -1);
+    io_uring_sqe_set_data(sqe, ctx);
+    
+    return 0;
+    cleanup:
+        if(ctx != NULL){
+            free(ctx->buf);
+            free(ctx);
+        }
+        return ret;
+}
+
+
 #if ENABLE_RBTREE
-void kvs_save_write_rbtree(rbtree *T, rbtree_node *node, FILE * fp) {
-    if(T == NULL || fp == NULL) return;
+void kvs_save_write_rbtree(rbtree *T, rbtree_node *node, int fd) {
+    if(T == NULL || fd < 0) return;
     int payload_length = 0;
 	if (node != T->nil) {
-        payload_length = strlen(node->key) + strlen((char *)node->value) + 2 + strlen("RSET");
-        //fprintf(fp, "RSET %s %s\r\n", node->key, (char *)node->value);
-        fprintf(fp, "%d*RSET %s %s\r\n", payload_length, node->key, (char *)node->value);
-		kvs_save_write_rbtree(T, node->left, fp);
-  
-		kvs_save_write_rbtree(T, node->right, fp);
+        kvs_io_uring_write(fd, node->key, node->value, KVS_RBTREE);
+
+		kvs_save_write_rbtree(T, node->left, fd);
+
+		kvs_save_write_rbtree(T, node->right, fd);
 	}
 
 }
 #endif
 
+
+
 int kvs_save_write(){
     if(global_config.enable_save == 0) return 0;
-    FILE * fp = fopen("./kvs-module/kvs_dump.rdb", "w+"); 
-    if(fp == NULL) return -2;
+
+    if(ring_save_inited == 0){
+    if(kvs_uring_init(ENTRY_LENGTH, &ring_save) != 0) {
+        return -1;
+    }
+    ring_save_inited = 1;
+}
+    int fd = open("./kvs-module/kvs_dump.rdb", O_WRONLY | O_CREAT | O_TRUNC, 0644); 
+    if(fd < 0) return -2;
+
+    tasks_count = 0;
+    
+
 #if ENABLE_RBTREE    
     kvs_rbtree_t * R_inst = &global_rbtree;
-    kvs_save_write_rbtree(R_inst, R_inst->root, fp);
+    kvs_save_write_rbtree(R_inst, R_inst->root, fd);
 #endif
 
 #if ENABLE_HASH
     kvs_hash_t * H_inst = &global_hash;
     if(H_inst->count > 0){
+        int count = H_inst->count;
         for (int i = 0;i < H_inst->max_slots;i ++) {
             hashnode_t *node = H_inst->nodes[i];
             while (node != NULL) { 
-                int payload_length = strlen(node->key) + strlen(node->value) + 2 + strlen("HSET");
-                //fprintf(fp, "HSET %s %s\r\n", node->key, node->value);
-                fprintf(fp, "%d*HSET %s %s\r\n", payload_length, node->key, (char *)node->value);
+                kvs_io_uring_write(fd, node->key, node->value, KVS_HASH);
                 node = node->next;
-                
+                --count;
             }
+            if(count <= 0) break;
         }   
     }
     
@@ -83,13 +247,14 @@ int kvs_save_write(){
 #if ENABLE_ARRAY
 
     kvs_array_t * inst = &global_array;
-    if(inst->table > 0){
+    if(inst->total > 0){
+        int count = inst->total;
         for (int i = 0;i < KVS_ARRAY_SIZE;i ++) {
             if (inst->table[i].key != NULL) {
-                int payload_length = strlen(inst->table[i].key) + strlen(inst->table[i].value) + 2 + strlen("SET");
-                //fprintf(fp, "SET %s %s\r\n", inst->table[i].key, inst->table[i].value);      
-                fprintf(fp, "%d*SET %s %s\r\n", payload_length, inst->table[i].key, inst->table[i].value);
+                kvs_io_uring_write(fd, inst->table[i].key, inst->table[i].value, KVS_ARRAY);
+                --count;
             }
+            if(count <= 0) break;
         }
     }
 
@@ -98,19 +263,32 @@ int kvs_save_write(){
 
 #if ENABLE_SKIPLIST
 
-        kvs_skiplist_t * L_inst = &global_skiplist;
-        Node * current = L_inst->header->forward[0];
-        while(current != NULL){
-            int payload_length = strlen(current->key) + strlen(current->value) + 2 + strlen("LSET");
-            fprintf(fp, "%d*LSET %s %s\r\n", payload_length, current->key, current->value);
-            current = current->forward[0];
-        }
+    kvs_skiplist_t * L_inst = &global_skiplist;
+    Node * current = L_inst->header->forward[0];
+    while(current != NULL){
+        kvs_io_uring_write(fd, current->key, current->value, KVS_SKIPLIST);
+        current = current->forward[0];
+    }
 
 
 #endif
+    io_uring_submit(&ring_save);
+    //printf("count: %d\n", tasks_count);
+    struct io_uring_cqe * cqe = NULL;
+    while(tasks_count > 0){
+        if(io_uring_peek_cqe(&ring_save, &cqe) == 0){
+            io_write_ctx * ctx = (io_write_ctx *)io_uring_cqe_get_data(cqe);
+            free(ctx->buf);
+            free(ctx);
+            io_uring_cqe_seen(&ring_save, cqe);
+            --tasks_count;
+        }
 
-    fflush(fp);
-    fclose(fp); 
+    }
+    //printf("count: %d\n", tasks_count);
+      
+    close(fd);
+
     return 0;
 
 }
@@ -118,42 +296,12 @@ int kvs_save_write(){
 
 
 
-int kvs_save_read(FILE * fp){
+int kvs_save_close(){
     if(global_config.enable_save == 0) return 0;
-    if(fp == NULL) return -1;
+    if(ring_save_inited == 1){      
+        io_uring_queue_exit(&ring_save);
+    } 
 
-    client_info cli = {0};
-    cli.w_cap = 128 - 1; 
-    cli.wbuf = (char *)malloc(128);
-    if(cli.wbuf == NULL) return -1;
-
-    cli.r_cap = 4096 - 1; 
-    cli.rbuf = (char *)malloc(4096);
-    if(cli.rbuf == NULL) {
-        free(cli.wbuf);
-        return -1;
-    }
-
-    int payload_length = 0;
-    while(fscanf(fp, "%d*", &payload_length) == 1){
-        if(cli.r_cap < payload_length){
-            char * temp = (char *)realloc(cli.rbuf, payload_length + 1);
-            if(temp == NULL){
-                break;
-            }
-            cli.rbuf = temp;
-            cli.r_cap = payload_length;
-        }
-
-        int len = fread(cli.rbuf, 1, payload_length, fp);
-        if(len == payload_length){
-            cli.rbuf[payload_length] = '\0';
-            kvs_handler(&cli);
-        }
-
-    }
-    free(cli.rbuf);
-    free(cli.wbuf);
     return 0;
 }
 
